b3f26ed569c37db37f99f5a5528d92bf6879c89e
maguofu <maguofu@sfmail.sf-express.com>
2021-04-20 16:59:29 +0800

    [update]低版本浏览器兼容

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh';
+// 传统浏览器可以通过插件 @vitejs/plugin-legacy 来支持，它将自动生成传统版本的 chunk 和其相应 ES 语言特性方面的 polyfill。
+// 兼容版的 chunk 只会在不支持原生 ESM 的浏览器中有按需加载。
 import legacyPlugin from '@vitejs/plugin-legacy';
 const { resolve } = require('path')
 
@@ -20,7 +22,12 @@ const { resolve } = require('path')
 export default ({ command, mode }) => {
   console.log(command, mode);
   let commonConfig = {
-    plugins: [reactRefresh(), legacyPlugin()],
+    plugins: [
+      reactRefresh(),
+      legacyPlugin({
+        targets: ['defaults', 'not IE 11']
+      }),
+  ],
   }
   if (command === 'serve') {
     return {
@@ -42,8 +49,8 @@ export default ({ command, mode }) => {
       build: {
         outDir: 'dist',
         assetsDir: 'assets',
-        sourcemap: true,
-        manifest: true,
+        // sourcemap: true,
+        // manifest: true,
       }
     }
   }
